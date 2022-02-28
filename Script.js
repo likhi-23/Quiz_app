0a9d12b135e7f6d15fb3ec9c2e903c44cc995133
Likhith karri <99127911+likhi-23@users.noreply.github.com>
2022-02-28 19:17:14 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,107 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+const stop = document.getElementById('stop-btn')
+const mssg = document.getElementById('mssg')
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
+
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
+}
+
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
+}
+
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    stop.classList.remove('hide')
+    stop.addEventListener('click', () => {
+      questionContainerElement.classList.add('hide')
+      stop.classList.add('hide')
+      mssg.innerHTML = "Submitted The Quiz"
+      startButton.innerText = 'Restart'
+      startButton.classList.remove('hide')
+      startButton.addEventListener('click',()=>{
+        mssg.innerHTML = ""
+      })
+
+    })
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+const questions = [
+  {
+    question: 'What Type Of JavaScript Is Used Here?',
+    answers: [
+      { text: 'Vanilla', correct: true },
+      { text: 'Vue', correct: false },
+      { text: 'React', correct: false },
+      { text: 'Angular', correct: false }
+    ]
+  },
+  {
+    question: 'Is web development fun?',
+    answers: [
+      { text: 'YES!!!', correct: true },
+      { text: 'Um no', correct: false }
+    ]
+  }
+]
